c8400f4a3f46c03799c45fc50c03ed2f59c6574e
Miguel Bassila <miguelbassila@me.com>
2015-06-23 23:53:51 -0300

    Add ProductClient to fetch requests

---
+++ Cart/Client/ProductClient.swift
@@ -0,0 +1,13 @@
+//
+//  ProductClient.swift
+//  Cart
+//
+//  Created by Miguel Bassila on 6/23/15.
+//  Copyright (c) 2015 Miguel Bassila. All rights reserved.
+//
+
+import Foundation
+
+struct ProductClient {
+  
+}
